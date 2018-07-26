55c2ad74eed3042a56a7113b91179561b12c51d2
eugeniogarzag <eugeniogarzagarcia@gmail.com>
2018-07-25 23:53:51 -0500

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,9 @@
 <?php
 // Check for empty fields
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
