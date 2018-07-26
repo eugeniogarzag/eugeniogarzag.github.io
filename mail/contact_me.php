8bcbbc96cc61e8483cea3001fec8f5d2a29c024e
eugeniogarzag <eugeniogarzagarcia@gmail.com>
2018-07-25 21:41:40 -0500

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -8,37 +8,12 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-
 // Create the email and send the message
-$to = "19garza5028@asfm.mx"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "yourname@yourdomain.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: 19martinez7395@asfm.mx\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
-
-
-//
-$host = "ssl://smtp.gmail.com";
-$username = "19garza5028@asfm.mx";
-$password = "19IAY0@o";
-$port = "465";
-$email_from = $email;
-$email_subject = $subject ;
-$email_body = $body ;
-$email_address = "19garza5028@asfm.mx";
-
-$headers = array ('From' => $email_from, 'To' => $to, 'Subject' => $email_subject, 'Reply-To' => $email_address);
-$smtp = Mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));
-$mail = $smtp->send($to, $headers, $email_body);
-
-
-if (PEAR::isError($mail)) {
-echo("<p>" . $mail->getMessage() . "</p>");
-} else {
-echo("<p>Message successfully sent!</p>");
-}
-
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
-
 ?>
